e59e99b76bf4f5f6a8d92a3cb7c890f747c3f716
Евгений Асадчий <asadchiyen@gmail.com>
2023-03-19 17:16:46 +0300

    Initial commit

---
+++ task3.py
@@ -0,0 +1,24 @@
+# 3. Создать текстовый файл (не программно), построчно записать фамилии сотрудников и величину их окладов.
+# Определить, кто из сотрудников имеет оклад менее 20 тыс., вывести фамилии этих сотрудников. Выполнить подсчет средней величины дохода сотрудников.
+
+def workers_statistics():
+    workers = [['Сидоров ', '10000 \n'], ['Кукушкин ', '17000 \n'], ['Иванов ', '140000 \n'], ['Смирнов ', '70000']]
+    try:
+        with open('new_file.txt', 'r+', encoding="utf-8") as file:
+            for i in range(len(workers)):
+                file.writelines(workers[i])
+            l = file.readlines()
+            poor = []
+            sum = 0
+            for i in range(len(l)):
+                salary = int((l[i].split())[1])
+                if salary < 20000:
+                    poor.append((l[i].split())[0])
+                sum += salary
+            print(f'Средняя величина дохода сотрудников равна {sum / len(workers) / 12:.2f}')
+            print(f'Меньше 20 тыс. рублей получает сотрудник: {", ".join(poor)}')
+    except FileNotFoundError:
+        return 'Файл не найден.'
+
+
+workers_statistics()
